1b90cdf38b92c0b291ffddc62ee5ce7cd5aabac8
tabishadnan <tabishadnan000@gmail.com>
2020-11-18 23:54:03 +0500

    styled category component

--- screens/GridCateogoryItemScreen.js
+++ screens/GridCateogoryItemScreen.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const GridCateogoryItemScreen = ({ navigation, cat }) => {
 
     return (
-        <View style={{
-            flex: 1,
-            backgroundColor: cat.item.color,
-            margin:10,
-            alignItems: 'center',
-            justifyContent: 'center',
-            color: "#141823",
-            height: 150,
-            borderWidth:2,
-            borderRadius:20,
-        }} onTouchStart={() => {
+        <TouchableOpacity onPress={() => {
             navigation.navigate("Category Meals Screen", { ctitle: cat.item.title });
         }}>
-            <Text style={{color : "#000000", fontSize:18,}}>{cat.item.title}</Text>
-        </View>
+            <View style={{
+                flex: 1,
+                backgroundColor: cat.item.color,
+                margin: 15,
+                alignItems: 'center',
+                justifyContent: 'center',
+                color: "#141823",
+                height: 150,
+                width: 150,
+                borderWidth: 2,
+                borderRadius: 20,
+            }}>
+                <Text style={{ color: "#000000", fontSize: 18, }}>{cat.item.title}</Text>
+            </View>
+        </TouchableOpacity>
     )
 };
 
